bc5008248f720b0b35d67c8bfd0293c5b61d59f8
Idan malka <Idanmal8@gmail.com>
2024-01-25 12:34:50 +0200

    keep working

--- lib/pages/my_team_list_screen/widget/pokemon_card_tile.dart
+++ lib/pages/my_team_list_screen/widget/pokemon_card_tile.dart
@@ -16,57 +16,63 @@ class PokemonCardTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
+        color: pokemon?.primaryTypeColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
         elevation: 2,
         child: GestureDetector(
           onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              children: [
-                pokemon != null
-                    ? Image.network(pokemon?.url ?? '', fit: BoxFit.contain)
-                    : const Icon(Icons.category_sharp),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      pokemon != null
-                          ? Text(pokemon!.name)
-                          : const Text('Pokemon name',
-                              overflow: TextOverflow.ellipsis),
-                      pokemon != null
-                          ? Text(pokemon!.types.first)
-                          : const Text(
-                              'Pokemon type',
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                    ],
-                  ),
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                // Flexible(
-                //   child: GridView.count(
-                //     shrinkWrap: true,
-                //     crossAxisCount: 2,
-                //     childAspectRatio: 3, // Adjust aspect ratio as needed
-                //     mainAxisSpacing: 4, // Adjust spacing as needed
-                //     crossAxisSpacing: 4, // Adjust spacing as needed
-                //     physics:
-                //         const NeverScrollableScrollPhysics(), // Disable scrolling inside the grid
-                //     children: const [
-                //       Text('firebalasdasdl', overflow: TextOverflow.ellipsis),
-                //       Text('fireball', overflow: TextOverflow.ellipsis),
-                //       Text('fireball', overflow: TextOverflow.ellipsis),
-                //       Text('fireball', overflow: TextOverflow.ellipsis),
-                //     ],
-                //   ),
-                // ),
-              ],
-            ),
+                child: pokemon != null
+                    ? Image.network(pokemon?.frontDefaultSprite ?? '',fit: BoxFit.fitWidth,)
+                    : const Icon(Icons.category_sharp),
+              ),
+          
+              const SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    pokemon != null
+                        ? Text(pokemon!.name)
+                        : const Text('Pokemon name',
+                            overflow: TextOverflow.ellipsis),
+                    pokemon != null
+                        ? Text(pokemon!.types.first)
+                        : const Text(
+                            'Pokemon type',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                  ],
+                ),
+              ),
+              // Flexible(
+              //   child: GridView.count(
+              //     shrinkWrap: true,
+              //     crossAxisCount: 2,
+              //     childAspectRatio: 3, // Adjust aspect ratio as needed
+              //     mainAxisSpacing: 4, // Adjust spacing as needed
+              //     crossAxisSpacing: 4, // Adjust spacing as needed
+              //     physics:
+              //         const NeverScrollableScrollPhysics(), // Disable scrolling inside the grid
+              //     children: const [
+              //       Text('firebalasdasdl', overflow: TextOverflow.ellipsis),
+              //       Text('fireball', overflow: TextOverflow.ellipsis),
+              //       Text('fireball', overflow: TextOverflow.ellipsis),
+              //       Text('fireball', overflow: TextOverflow.ellipsis),
+              //     ],
+              //   ),
+              // ),
+            ],
           ),
         ),
       ),
